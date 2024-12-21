9507a0a667e0fad1efd4056c3fb57bfd2cc119ea
ImranMallik <imran80mallik@gmail.com>
2024-12-21 09:54:23 +0530

    Desing Add to caet them

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,7 @@
+<x-app-layout>
+    <section class="wsus__product mt_145 pb_100">
+        <div class="container">
+            <div class="alert alert-success">It's Working</div>
+        </div>
+    </section>
+</x-app-layout>
---
+++ resources/views/profile/edit.blade.php
@@ -0,0 +1,17 @@
+<x-app-layout>
+    <section class="wsus__product mt_145 pb_100">
+        <div class="container">
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="">
+                    @include('profile.partials.update-profile-information-form')
+                </div>
+            </div>
+            <br>
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="">
+                    @include('profile.partials.update-password-form')
+                </div>
+            </div>
+        </div>
+    </section>
+</x-app-layout>
